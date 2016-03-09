07210cacb6a25412340527a623224338a462713e
Federico Melaccio <federico.melaccio@stfc.ac.uk>
2016-03-09 17:17:36 +0000

    Changing the name of bwctl server according to version 3.5.1 rules

--- manifests/bwctl/service.pp
+++ manifests/bwctl/service.pp
@@ -2,10 +2,9 @@ class perfsonar::bwctl::service(
   $ensure = $::perfsonar::params::bwctl_ensure,
   $enable = $::perfsonar::params::bwctl_enable,
 ) inherits perfsonar::params {
-  service { 'bwctld':
+  service { 'bwctl-server':
     ensure     => $ensure,
     enable     => $enable,
-    hasstatus  => false,
     hasrestart => true,
     require    => Package['bwctl-server'],
   }
