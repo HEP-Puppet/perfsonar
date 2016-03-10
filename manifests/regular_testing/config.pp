1122d4652b52d9960365a3a24859b13b7edfde99
Federico Melaccio <federico.melaccio@stfc.ac.uk>
2016-03-10 15:58:09 +0000

    Adding some useful parameters to customize install/config paths

--- manifests/regular_testing/config.pp
+++ manifests/regular_testing/config.pp
@@ -1,11 +1,13 @@
 # loglevel is a puppet metaparameter, so have to use something else (loglvl)
 class perfsonar::regular_testing::config(
+  $libpath    = $::perfsonar::params::regular_testing_libpath,
   $snotify    = $::perfsonar::params::regular_testing_snotify,
   $loglvl     = $::perfsonar::params::regular_testing_loglvl,
   $logger     = $::perfsonar::params::regular_testing_logger,
   $logfile    = $::perfsonar::params::regular_testing_logfile,
   $configfile = $::perfsonar::params::regular_testing_config,
   $esmondroot = $::perfsonar::params::esmond_root,
+  $esmondconf = $::perfsonar::params::esmond_conf_path,
 ) inherits perfsonar::params {
   file { '/usr/local/sbin/puppet_perfsonar_configure_regular_testing':
     ensure  => 'file',
