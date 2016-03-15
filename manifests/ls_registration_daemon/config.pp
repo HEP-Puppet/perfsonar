2bc8f58738b5f8e5609eb15debe8449f61594a83
Federico Melaccio <federico.melaccio@stfc.ac.uk>
2016-03-15 12:50:05 +0000

    Rewriting ls_registration_daemon config and admin info template to append infos to the right file, /etc/perfsonar/lsregistrationdaemon.conf, as administrative_info is currently ignored

--- manifests/ls_registration_daemon/config.pp
+++ manifests/ls_registration_daemon/config.pp
@@ -1,8 +1,9 @@
 class perfsonar::ls_registration_daemon::config(
-  $snotify = $::perfsonar::params::ls_registration_daemon_snotify,
-  $loglvl  = $::perfsonar::params::ls_registration_daemon_loglvl,
-  $logger  = $::perfsonar::params::ls_registration_daemon_logger,
-  $logfile = $::perfsonar::params::ls_registration_daemon_logfile,
+  $snotify    = $::perfsonar::params::ls_registration_daemon_snotify,
+  $loglvl     = $::perfsonar::params::ls_registration_daemon_loglvl,
+  $logger     = $::perfsonar::params::ls_registration_daemon_logger,
+  $logfile    = $::perfsonar::params::ls_registration_daemon_logfile,
+  $admininfo  = {},
 ) inherits perfsonar::params {
   $tn = $snotify ? {
     false   => undef,
@@ -15,5 +16,12 @@ class perfsonar::ls_registration_daemon::config(
     mode    => '0644',
     content => template("${module_name}/log4perl-logger.conf.erb"),
     notify  => $tn,
+  }
+  
+  # TODO: use Augeas to write this config instead of a cat
+  exec { 'append_info':
+    command => '/bin/cat /etc/perfsonar/toolkit/administrative_info >> /etc/perfsonar/lsregistrationdaemon.conf',
+    unless  => '/bin/grep site_project /etc/perfsonar/lsregistrationdaemon.conf > /dev/null',
+    require => File['/etc/perfsonar/toolkit/administrative_info'],
   }
 }
